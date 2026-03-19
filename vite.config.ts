d9ae312b8412e8bb77bbec550dcdae9f7ba4f364
Bruno <bruno.boulandet@scalingo.com>
2026-03-19 12:16:05 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import { componentTagger } from "lovable-tagger";
 export default defineConfig(({ mode }) => ({
   server: {
     host: "0:0:0.0",
-    port: 8080,
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
